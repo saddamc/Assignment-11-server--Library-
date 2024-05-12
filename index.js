b8ed1ff3b50371526e4cb034c0f006f227024cd9
saddamcall <saddam13bd@gmail.com>
2024-05-12 23:44:29 +0600

    book borrow data succesfully add mongoDB & user wise data show done

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // const jwt = require('jsonwebtoken');  /**token related import */
 // const cookieParser = require('cookie-parser'); 
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -56,7 +55,7 @@ async function run() {
 
        const options = {
             // Include only the `title` and `imdb` fields in the returned document
-            projection: { name: 1, author: 1, book_id: 1, image: 1, category: 1, rating: 1 },
+            projection: { book: 1, author: 1, book_id: 1, image: 1, category: 1, rating: 1 },
     };
 
       const result = await bookCollection.findOne(query);
@@ -70,7 +69,7 @@ async function run() {
       if(req.query?.email){
         query = {email: req.query.email}
       }
-      const result =await borrowedCollection.find().toArray();
+      const result =await borrowedCollection.find(query).toArray();
       res.send(result);
     })
 
